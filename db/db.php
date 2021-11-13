7a4c10e25c861cda3d99ac7f480fc4da6cd10660
graeme <grae0026@stud.kea.dk>
2021-11-13 16:57:52 +0100

    fix: db credentials security ini file

--- db/db.php
+++ db/db.php
@@ -1,16 +1,18 @@
 <?php
 try {
-  $sDatabaseUserName = 'honeypot';
-  // $sDatabaseUserName = 'admin';
-  $sDatabasePassword = 'strongpass100';
-  // $sDatabasePassword = 'S3qu3ntial!';
-  $sDatabaseConnection = "mysql:host=localhost:3306; dbname=web_security; charset=utf8mb4";
+  $DatabaseAccess = parse_ini_file($_SERVER['DOCUMENT_ROOT'] . '/config/access.ini');
+  // print_r($DatabaseAccess);
+  $dbpass = $DatabaseAccess['dbpass'];
+  $dbhost = $DatabaseAccess['dbhost'];
+  $dbname = $DatabaseAccess['dbname'];
+  $dbuser = $DatabaseAccess['dbuser'];
+  $sDatabaseConnection = "mysql:host=$dbhost; dbname=$dbname; charset=utf8mb4";
   $aDatabaseOptions = array(
     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ // Array with object
   );
-  $db = new PDO($sDatabaseConnection, $sDatabaseUserName, $sDatabasePassword, $aDatabaseOptions);
+  $db = new PDO($sDatabaseConnection, $dbuser, $dbpass, $aDatabaseOptions);
 } catch (PDOException $e) {
   echo 'Connectin failed:' . $e->getMessage();
   exit();
-}
+}  
